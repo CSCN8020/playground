d4221e8ed6b3e15c5e4d2a2a2249995a29c584d1
Mahmoud Nasr <mahmoudmnasr95@gmail.com>
2024-03-16 13:01:36 -0400

    Lec10: Add tensorboard

--- lec10_actor_critic/actor_critic_solved.py
+++ lec10_actor_critic/actor_critic_solved.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import time
+import datetime
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -12,6 +13,7 @@ from tensorflow.keras import layers
 
 class modelA2C:
     def __init__(self, num_inputs, num_hidden, num_actions):
+        self.num_actions = num_actions
         inputs = layers.Input(shape=(num_inputs,))
         common = layers.Dense(num_hidden, activation="relu")(inputs)
         # Actor: action
@@ -29,10 +31,12 @@ class modelA2C:
         # from environment state
         state_tensor = tf.convert_to_tensor(state)
         state_tensor = tf.expand_dims(state_tensor, 0)
+        # Use the model with the state to obtain action probabilities and critic value
         action_probs, critic_value = self.model(state_tensor)
 
         # Sample action from action probability distribution
-        action = np.random.choice(num_actions, p=np.squeeze(action_probs))
+        # Choose an action. 
+        action = np.random.choice(self.num_actions, p=np.squeeze(action_probs))
         return action_probs, critic_value, action
     
     '''@brief Return the value with the max probability
@@ -46,7 +50,8 @@ class modelA2C:
         action_probs, _ = self.model(state_tensor)
         return np.argmax(action_probs)
     
-    def update_model(self, history, tape):
+    def update_model(self, history, tape, file_writer, epoch):
+        # history : log_action_probs_history, critic_value_history, returns
         actor_losses = []
         critic_losses = []
         for log_prob, value, ret in history:
@@ -55,17 +60,26 @@ class modelA2C:
             # of `log_prob` and ended up recieving a total reward = `ret`.
             # The actor must be updated so that it predicts an action that leads to
             # high rewards (compared to critic's estimate) with high probability.
-            diff = ret - value
-            actor_losses.append(-log_prob * diff)  # actor loss
+            # A = reward + gamma*V(St+1) - V(S)
+            # A = return - V(s)
+            delta = ret - value
+
+            # TODO: Calculate the actor_loss
+            actor_loss = -log_prob*delta
+            actor_losses.append(actor_loss)  # actor loss
 
             # The critic must be updated so that it predicts a better estimate of
             # the future rewards.
+            # TODO calculate crtic loss based on huber loss
+            critic_loss = self.huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
             critic_losses.append(
-                self.huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+                critic_loss
             )
-
         # Backpropagation
         loss_value = sum(actor_losses) + sum(critic_losses)
+        with file_writer.as_default():
+            tf.summary.scalar('loss', loss_value, step = epoch)
+
         grads = tape.gradient(loss_value, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
 
@@ -93,14 +107,16 @@ def test(model, ckpt, n_episodes=1, seed=42):
                 break
             
 
-def train(model, ckpt = "", seed=42, max_steps_per_episode=10000, model_ckpt_name="", visualize_training=False):
+def train(model, load_model_ckpt="", model_ckpt = "", seed=42, max_steps_per_episode=10000, model_ckpt_name="", visualize_training=False):
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_writer = tf.summary.create_file_writer(f"cartpole_tensorboard/{current_time}")
     if visualize_training:
         env = gym.make("CartPole-v1", render_mode="human")  # Create the environment
     else:
         env = gym.make("CartPole-v1")
     
-    if ckpt != "":
-        model.load_weights(ckpt)
+    if load_model_ckpt != "":
+        model.load_weights(load_model_ckpt)
     
     action_probs_history = []
     critic_value_history = []
@@ -146,7 +162,7 @@ def train(model, ckpt = "", seed=42, max_steps_per_episode=10000, model_ckpt_nam
 
             # Calculating loss values to update our network
             history = zip(action_probs_history, critic_value_history, returns)
-            model.update_model(history, tape)
+            model.update_model(history, tape, file_writer, episode_count)
 
             # Clear the loss and reward history
             action_probs_history.clear()
@@ -154,15 +170,19 @@ def train(model, ckpt = "", seed=42, max_steps_per_episode=10000, model_ckpt_nam
             rewards_history.clear()
 
         # Log details
+        with file_writer.as_default():
+            tf.summary.scalar('running_reward', running_reward, step = episode_count)
+            tf.summary.scalar('episode_reward', episode_reward, step = episode_count)
+
         episode_count += 1
         if episode_count % 10 == 0:
             template = "running reward: {:.2f} at episode {}"
             print(template.format(running_reward, episode_count))
 
-        if running_reward > 195:  # Condition to consider the task solved
+        if running_reward > 400:  # Condition to consider the task solved
             print("Solved at episode {}!".format(episode_count))
-            # if model_ckpt_name != "":
-            #     model.save_weights(model_ckpt_name)
+            if model_ckpt != "":
+                model.save_weights(model_ckpt)
             break
 
 
@@ -177,12 +197,15 @@ if __name__=="__main__":
     num_inputs = 4
     num_actions = 2
     num_hidden = 128
-    model_name = "ckpt/model_cartpolev1.ckpt"
-    visualize_training = True
+    save_model_name = "ckpt/model_cartpolev1.ckpt"
+    load_model_name = "" # "" --> No model to load
+    visualize_training = False
     model = modelA2C(num_inputs, num_hidden, num_actions)
     if MODE == "TRAIN":
         train(model, seed=seed, max_steps_per_episode=max_steps_per_episode, 
-              ckpt=model_name,
+              model_ckpt=save_model_name,
               visualize_training=visualize_training)
     elif MODE == "TEST":
-        test(model, ckpt=model_name, n_episodes=3, seed=seed)
+        test(model, ckpt=load_model_name, n_episodes=3, seed=seed)
+
+    # test(model, ckpt=model_name, n_episodes=3, seed=seed)
